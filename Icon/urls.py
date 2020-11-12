caca4fc250d483ea02d559f826bce28cbbcbbacd
kouduki1101 <soundtrackfavor@gmail.com>
2020-11-12 17:57:36 +0900

    11/12 PM

--- Icon/urls.py
+++ Icon/urls.py
@@ -8,9 +8,7 @@ app_name = 'Icon'
 urlpatterns = [
 
     path('search', search, name='search'),
-
-
-
+    path('', search, name='search'),#とりあえずの設定
 
     path('table/new', table.TableCreateView.as_view(), name='table_new'),
     path('table/<int:pk>/column/new', column.ColumnCreateView.as_view(), name='col_new'),  # 追加
