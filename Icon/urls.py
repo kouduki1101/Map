28cbc0609121ed52f480c62ac4bdd6035834e653
kouduki1101 <soundtrackfavor@gmail.com>
2020-11-19 17:22:28 +0900

    11/19 17:30

--- Icon/urls.py
+++ Icon/urls.py
@@ -13,8 +13,16 @@ urlpatterns = [
     path('signup', signupview, name='signup'),
     path('login/', loginview, name='login'),
     path('list/', list, name='list'),
-    path('change_password/', auth_views.PasswordChangeView.as_view(), name='change_password'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    #
+
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     path('email/', emailfunc),
 
